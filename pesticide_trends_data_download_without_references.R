269b07553cf70608b221b0e13982089b9ff52389
seschaub <44777479+seschaub@users.noreply.github.com>
2020-06-10 08:59:35 +0200

    Update pesticide_trends_data_download_without_references.R

--- pesticide_trends_data_download_without_references.R
+++ pesticide_trends_data_download_without_references.R
@@ -1,8 +1,13 @@
 ################################################################################
 # R Code related to                                                            #
 # Tracking societal concerns on pesticides – A Google Trends analysis          #
+# by Sergei Schaub, Robert Huber & Robert Finger                               # 
+# 2020                                                                         #
+# Environmental Research Letters                                               #
 # -----------------------------------------------------------------------------#
 # r script for downloading and first preparation of data                       #
+# 2020, ETH Zurich                                                             #
+# developed by Sergei Schaub                                                   #  
 ################################################################################
 
 # table of content:
